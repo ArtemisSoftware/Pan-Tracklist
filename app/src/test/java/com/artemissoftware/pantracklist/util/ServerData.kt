aaa2eb369f8647da882b4344f175a58a0b558c05
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-09-29 22:37:21 +0100

    ServerData

---
+++ app/src/test/java/com/artemissoftware/pantracklist/util/ServerData.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.pantracklist.util
+
+object ServerData {
+    const val ALBUMS_RESPONSE = "albums.json"
+    const val ERROR_RESPONSE = "error.json"
+    const val ERROR_ALBUMS_WITH_MISSING_FIELDS_RESPONSE = "error_albums_with_missing_fields.json"
+}
