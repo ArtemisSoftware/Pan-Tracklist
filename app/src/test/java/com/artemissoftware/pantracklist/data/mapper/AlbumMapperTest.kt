139f519760df8064f256177b9bb545c01c57778b
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-09-29 19:43:07 +0100

    AlbumMapperTest

---
+++ app/src/test/java/com/artemissoftware/pantracklist/data/mapper/AlbumMapperTest.kt
@@ -0,0 +1,23 @@
+package com.artemissoftware.pantracklist.data.mapper
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.artemissoftware.pantracklist.util.TestData.albumList
+import com.artemissoftware.pantracklist.util.TestData.albumListDto
+import com.artemissoftware.pantracklist.util.TestData.albumListEntities
+import org.junit.jupiter.api.Test
+
+class AlbumMapperTest {
+
+    @Test
+    fun `Map AlbumDto to AlbumEntity`(){
+        assertThat(albumListDto.first().toEntity())
+            .isEqualTo(albumListEntities.first())
+    }
+
+    @Test
+    fun `Map AlbumEntity to Album`(){
+        assertThat(albumListEntities.first().toAlbum())
+            .isEqualTo(albumList.first())
+    }
+}
