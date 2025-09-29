a8fb9fc7db58c6a82916184996f0a7cee32159fa
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-09-29 19:04:40 +0100

    AlbumDto

---
+++ app/src/main/java/com/artemissoftware/pantracklist/data/network/dto/AlbumDto.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.pantracklist.data.network.dto
+
+
+import com.squareup.moshi.Json
+
+data class AlbumDto(
+    @field:Json(name = "albumId")
+    val albumId: Int,
+    @field:Json(name = "id")
+    val id: Int,
+    @field:Json(name = "thumbnailUrl")
+    val thumbnailUrl: String,
+    @field:Json(name = "title")
+    val title: String,
+    @field:Json(name = "url")
+    val url: String
+)
