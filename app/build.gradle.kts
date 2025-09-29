547cb5086e08d5f4cf67216a25d953edc0976ca2
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-09-29 18:59:25 +0100

    retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
