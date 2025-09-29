e893ade27a72bbf7709c0f0090ee220de98e444e
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-09-29 20:00:47 +0100

    Resource

---
+++ app/src/main/java/com/artemissoftware/pantracklist/core/domain/Resource.kt
@@ -0,0 +1,18 @@
+package com.artemissoftware.pantracklist.core.domain
+
+import com.artemissoftware.pantracklist.core.domain.error.Error
+
+sealed interface Resource<T> {
+    data class Success<T>(val data: T) : Resource<T>
+    data class Failure<T>(val error: Error) : Resource<T>
+
+    fun onSuccess(block: (T) -> Unit): Resource<T> {
+        if (this is Success) block(data)
+        return this
+    }
+
+    fun onFailure(block: (Error) -> Unit): Resource<T> {
+        if (this is Failure) block(error)
+        return this
+    }
+}
