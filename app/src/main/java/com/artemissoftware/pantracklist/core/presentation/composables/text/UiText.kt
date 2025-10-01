b60f928c43d340b754c33c635073398c8a1d727b
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-01 12:22:31 +0100

    UiText

---
+++ app/src/main/java/com/artemissoftware/pantracklist/core/presentation/composables/text/UiText.kt
@@ -0,0 +1,33 @@
+package com.artemissoftware.pantracklist.core.presentation.composables.text
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
+
+@Parcelize
+sealed interface UiText: Parcelable {
+    data class DynamicString(val value: String): UiText
+    class StringResource(
+        @StringRes val id: Int,
+        vararg val args: @RawValue Any,
+    ): UiText
+
+    @Composable
+    fun asString(): String {
+        return when(this) {
+            is DynamicString -> value
+            is StringResource -> stringResource(id = id, *args)
+        }
+    }
+
+    fun asString(context: Context): String {
+        return when(this) {
+            is DynamicString -> value
+            is StringResource -> context.getString(id, *args)
+        }
+    }
+}
