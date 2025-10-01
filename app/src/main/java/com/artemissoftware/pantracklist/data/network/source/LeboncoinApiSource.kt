b1ba0306a91fc8b765e7e4988ffe9f98a85c3bb1
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-01 18:13:30 +0100

    RepositoryModule

--- app/src/main/java/com/artemissoftware/pantracklist/data/network/source/LeboncoinApiSource.kt
+++ app/src/main/java/com/artemissoftware/pantracklist/data/network/source/LeboncoinApiSource.kt
@@ -3,8 +3,9 @@ package com.artemissoftware.pantracklist.data.network.source
 import com.artemissoftware.pantracklist.data.network.HandleApi
 import com.artemissoftware.pantracklist.data.network.LeboncoinApi
 import com.artemissoftware.pantracklist.data.network.dto.AlbumDto
+import javax.inject.Inject
 
-class LeboncoinApiSource constructor(private val leboncoinApi: LeboncoinApi) {
+class LeboncoinApiSource @Inject constructor(private val leboncoinApi: LeboncoinApi) {
 
     suspend fun getAlbums(): List<AlbumDto> {
         return HandleApi.safeApiCall { leboncoinApi.getAlbums() }
