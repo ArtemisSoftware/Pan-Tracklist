5e146b6b8f0a1ce337cced676aff598895d71567
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-03 19:20:57 +0100

    AlbumsScreenTest

---
+++ app/src/androidTest/java/com/artemissoftware/pantracklist/features/albums/presentation/AlbumsScreenTest.kt
@@ -0,0 +1,89 @@
+package com.artemissoftware.pantracklist.features.albums.presentation
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.artemissoftware.pantracklist.core.designsystem.theme.PanTracklistTheme
+import com.artemissoftware.pantracklist.features.albums.presentation.albums.AlbumsScreen
+import com.artemissoftware.pantracklist.test.TestActivity
+import com.artemissoftware.pantracklist.util.PanTracklistAndroidTest
+import com.artemissoftware.pantracklist.util.dispatcher.MockServerDispatcher
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class AlbumsScreenTest: PanTracklistAndroidTest() {
+
+    @get:Rule
+    val composeRule = createAndroidComposeRule<TestActivity>()
+
+    private lateinit var robot: AlbumsScreenRobot<TestActivity>
+
+    override fun setUp() {
+        super.setUp()
+        robot = AlbumsScreenRobot(composeRule)
+    }
+
+    @Test
+    fun load_albums() = runBlocking<Unit>{
+
+        mockServer.dispatcher = MockServerDispatcher().successDispatcher()
+
+        composeRule.setContent {
+            PanTracklistTheme {
+                AlbumsScreen()
+            }
+        }
+
+        composeRule.awaitIdle()
+
+        robot
+            .assertAlbumListIsDisplayed()
+    }
+
+    @Test
+    fun load_albums_with_error() = runBlocking<Unit>{
+
+        mockServer.dispatcher = MockServerDispatcher().errorDispatcher()
+
+        composeRule.setContent {
+            PanTracklistTheme {
+                AlbumsScreen()
+            }
+        }
+
+        composeRule.awaitIdle()
+
+        robot
+            .assertErrorPlaceHolderDisplayed()
+    }
+
+    @Test
+    fun load_albums_with_error_reload_data_get_albums() = runBlocking<Unit>{
+
+        mockServer.dispatcher = MockServerDispatcher().errorDispatcher()
+
+        composeRule.setContent {
+            PanTracklistTheme {
+                AlbumsScreen()
+            }
+        }
+
+        composeRule.awaitIdle()
+
+        robot
+            .assertErrorPlaceHolderDisplayed()
+
+
+        mockServer.dispatcher = MockServerDispatcher().successDispatcher()
+
+        robot
+            .performErrorPlaceHolderClick()
+
+        composeRule.awaitIdle()
+
+        robot
+            .assertAlbumListIsDisplayed()
+
+    }
+}
