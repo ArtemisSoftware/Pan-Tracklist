5135bc6c39ba9bc0566caceea8529acdac0a3f3f
Gustavo Isaac <gustavo.isaac@gmail.com>
2025-10-03 12:25:09 +0100

    TestDatabaseModule

---
+++ app/src/androidTest/java/com/artemissoftware/pantracklist/core/database/di/TestDatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.artemissoftware.pantracklist.core.database.di
+
+import android.content.Context
+import androidx.room.Room
+import com.artemissoftware.pantracklist.core.database.PanTracklistDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DataBaseModule::class]
+)
+@Module
+class TestDatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideTestDB(@ApplicationContext context: Context): PanTracklistDatabase {
+        return Room.inMemoryDatabaseBuilder(
+            context,
+            PanTracklistDatabase::class.java
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAlbumDao(database: PanTracklistDatabase) = database.getAlbumDao()
+}
